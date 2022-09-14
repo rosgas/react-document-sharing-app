a93fd185b7331c027fa87a23d5988aadb3b1d185
rosgas <rgasbarri.274@gmail.com>
2022-09-14 13:27:12 +1000

    ProfilePage/Update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,40 @@
+import { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
+import { Link, useNavigate } from "react-router-dom";
+
 function Profile() {
-  return <div>Profile</div>;
+  const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    jobTitle: auth.currentUser.jobTitle,
+    email: auth.currentUser.email,
+  });
+
+  const { name, jobTitle, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut(navigate("/"));
+  };
+
+  return (
+    <div className="profile">
+      <header>
+        <h2>My profile</h2>
+        <button type="button" className="btn btn-main" onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+    </div>
+  );
 }
 
 export default Profile;
+
+/* function Profile() {
+  return <></>;
+}
+
+export default Profile; */
