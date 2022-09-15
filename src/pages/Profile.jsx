0e115c17961c0e8b57c580dc50384e095e82d9ed
rosgas <rgasbarri.274@gmail.com>
2022-09-15 13:05:37 +1000

    Fetch user form data

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,17 +1,38 @@
 import { useState, useEffect } from "react";
-import { getAuth } from "firebase/auth";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase.config";
 
 function Profile() {
   const auth = getAuth();
-  const [changeDetails, setChangeDetails] = useState();
-  const [formData, setFormData] = useState({
-    name: auth.currentUser.displayName,
-    jobTitle: auth.currentUser.jobTitle,
-    email: auth.currentUser.email,
+  const [userDetails, setUserDetails] = useState({
+    fullName: "",
+    jobTitle: "",
+    email: "",
   });
 
-  const { name, jobTitle, email } = formData;
+  const { fullName, jobTitle, email } = userDetails;
+
+  useEffect(() => {
+    const fetchuserFormData = async () => {
+      const userRef = doc(db, "users", auth.currentUser.uid);
+      const docSnap = await getDoc(userRef);
+      const userData = docSnap.data().userFormData;
+
+      if (docSnap.exists()) {
+        setUserDetails({
+          fullName: userData.name,
+          jobTitle: userData.jobTitle,
+          email: userData.email,
+        });
+      } else {
+        console.log("No such document!");
+      }
+    };
+
+    fetchuserFormData();
+  }, [auth.currentUser.uid]);
 
   const navigate = useNavigate();
 
@@ -23,6 +44,9 @@ function Profile() {
     <div className="profile">
       <header>
         <h2>My profile</h2>
+        <p>{fullName}</p>
+        <p>{jobTitle}</p>
+        <p>{email}</p>
         <button type="button" className="btn btn-main" onClick={onLogout}>
           Logout
         </button>
