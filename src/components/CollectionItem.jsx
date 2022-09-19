f59b21bfee7462e250dd53b54184d9c92fdbf282
rosgas <rgasbarri.274@gmail.com>
2022-09-20 08:24:17 +1000

    CollectionItem comp/update

--- src/components/CollectionItem.jsx
+++ src/components/CollectionItem.jsx
@@ -1,5 +1,4 @@
-import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import moment from "moment";
 
 function CollectionItem({ item, id, setIsShown }) {
