f32bda46bbe8f8b62d419746a1e50556107f63c4
rosgas <rgasbarri.274@gmail.com>
2022-09-20 08:29:21 +1000

    NotificatiosnBell comp/update

--- src/components/NotificationsBell.jsx
+++ src/components/NotificationsBell.jsx
@@ -1,7 +1,7 @@
 import bell from "../assets/svg/bell.svg";
 
 function NotificationsBell() {
-  return <img src={bell} alt="" />;
+  return <img src={bell} alt="Notification" style={{ cursor: "pointer" }} />;
 }
 
 export default NotificationsBell;
