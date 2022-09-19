7a3cb4b7daea251fcecd1774a9ae05f265119b2d
rosgas <rgasbarri.274@gmail.com>
2022-09-20 07:07:55 +1000

    Create CommentBox component

---
+++ src/components/CommentsBox.jsx
@@ -0,0 +1,12 @@
+function CommentsBox({ docId }) {
+  const id = docId;
+  return (
+    <>
+      <div className="comments-box">
+        <h5 className="fs-20 fw-400">Comments</h5>
+      </div>
+    </>
+  );
+}
+
+export default CommentsBox;
